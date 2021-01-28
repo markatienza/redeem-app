d2f3c3251989cb4e32c105eff56568a233b0cb94
markatienza <atienza.markangelo26@gmailcom>
2021-01-28 09:42:38 +0800

    Add home link to Logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="/home">
         <img src="@/assets/logo.png" alt="" width="40" height="35" />
         REDEEM APP
       </a>
